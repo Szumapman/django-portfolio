1ba3a289d3731b67dd1ddc258582a0d67e3e848e
Paweł Szumański <pawel.szumanski@outlook.com>
2025-01-08 11:15:31 +0100

    create project_list

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def project_list(request):
+    # return render(request, "projects/project_list.html")
+    return HttpResponse("Hello World!")
