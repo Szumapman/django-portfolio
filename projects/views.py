2e84e03ad68915dcc3a5244b00ef1b06a3243fbf
Paweł Szumański <pawel.szumanski@outlook.com>
2025-01-12 18:14:38 +0100

    add project_detail

--- projects/views.py
+++ projects/views.py
@@ -13,3 +13,8 @@ def all_projects(request):
     # query the database to return all projects objects
     projects = Project.objects.all()
     return render(request, "projects/all_projects.html", {"projects": projects})
+
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, "projects/detail.html", {"project": project})
