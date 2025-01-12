fd382efd6e3b8b99af51c12b4f08e4a3bf6abba4
Paweł Szumański <pawel.szumanski@outlook.com>
2025-01-12 17:37:21 +0100

    add app_projects view

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from projects.models import Project
+
 
 # Create your views here.
-def project_list(request):
-    return render(request, "projects/index.html")
+
+# def project_list(request):
+#     return render(request, "projects/index.html")
+
+
+def all_projects(request):
+    # query the database to return all projects objects
+    projects = Project.objects.all()
+    return render(request, "projects/all_projects.html", {"projects": projects})
